ba0eacf614b47a739649d98d66df1ad681eafd73
Scripts-Service <pre.mos_os@icloud.com>
2016-10-04 10:49:45 +0700

    fix config to UID

--- application/models/Firebase.php
+++ application/models/Firebase.php
@@ -3,8 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 const DEFAULT_URL = 'https://test-82d9b.firebaseio.com/';
 class Firebase extends CI_Model {
 	public function get(){
+		$UID = $this->config->item('UID');
 		$firebase = new \Firebase\FirebaseLib(DEFAULT_URL);
-		$data = $firebase->get("UserData/$this->config->item('UID')/AccountData/");
+		$data = $firebase->get("UserData/$UID/AccountData/");
 		if(json_decode($data) == null){
 			return false;
 		}else{
